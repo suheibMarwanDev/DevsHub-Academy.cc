7228fae3abf7c86084854543b594758a7f0118f5
Suhaib Marwan Rasheed <suheibmarwaneng@gmail.com>
2026-09-21 12:05:22 +0300

    Add admin logout endpoint

---
+++ api/auth/logout.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const { clearSessionCookies } = require("../lib/auth");
+
+function json(res, status, body) {
+  res.statusCode = status;
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  res.setHeader("Cache-Control", "no-store");
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.end(JSON.stringify(body));
+}
+
+module.exports = async function handler(req, res) {
+  if (req.method !== "POST") {
+    res.setHeader("Allow", "POST");
+    return json(res, 405, { error: "Method not allowed" });
+  }
+
+  clearSessionCookies(res);
+  return json(res, 200, { authenticated: false });
+};
